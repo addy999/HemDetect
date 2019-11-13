d3ba322728a91e0a535f3a0026056434413f5094
addy999 <addy@loftorbital.com>
2019-11-13 13:23:25 -0500

    update cuda script

--- cuda_install.sh
+++ cuda_install.sh
@@ -3,8 +3,8 @@ echo "Checking for CUDA and installing."
 # Check for CUDA and try to install.
 if ! dpkg-query -W cuda; then
   # The 16.04 installer works with 16.10.
-  curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-  dpkg -i ./cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+  curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_10.1.243-1_amd64.deb
+  dpkg -i ./cuda-repo-ubuntu1604_10.1.243-1_amd64.deb
   apt-get update
   apt-get install cuda -y
 fi
@@ -15,8 +15,10 @@ echo 'export LD_LIBRARY_PATH=$CUDA_HOME/lib64' >> ~/.bashrc
 source ~/.bashrc
 
 cd $HOME
-tar xzvf cudnn-8.0-linux-x64-v5.1.tgz
+echo "Downloading cudnn"
+curl -o https://developer.nvidia.com/compute/machine-learning/cudnn/secure/7.6.5.32/Production/10.1_20191031/cudnn-10.1-linux-x64-v7.6.5.32.tgz
+tar xzvf cudnn-10.1-linux-x64-v7.6.5.32.tgz
 sudo cp cuda/lib64/* /usr/local/cuda/lib64/
 sudo cp cuda/include/cudnn.h /usr/local/cuda/include/
 rm -rf ~/cuda
-rm cudnn-8.0-linux-x64-v5.1.tgz
+rm cudnn-10.1-linux-x64-v7.6.5.32.tgz
