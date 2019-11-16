5c6ca2f9bb9ee8c3e69ae0d45bd51fa34d741d2c
addy999 <addy@loftorbital.com>
2019-11-15 23:38:04 -0500

    misc

--- Training/Scripts/DetectionModels.py
+++ Training/Scripts/DetectionModels.py
@@ -55,7 +55,7 @@ class AlexNetDetector2(nn.Module):
 resnet152 = torchvision.models.resnet152(pretrained=True)
 modules = list(resnet152.children())
 modules[0].in_channels = 1
-resnet152 = nn.Sequential(modules)
+resnet152 = nn.Sequential(*modules)
 for param in resnet152.parameters():
     param.requires_grad = False
 
